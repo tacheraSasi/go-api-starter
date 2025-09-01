ddd22098904c8672c9401c6d8e8833ad06405b51
Tachera Sasi <tacherasasi@gmail.com>
2025-09-01 14:35:30 +0300

    Implement database connection management with GORM

---
+++ pkg/database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func Connect(host, port, user, password, dbname string) error {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %v", err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	DB = db
+	log.Println("Database connected successfully")
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
+
+func AutoMigrate() {}

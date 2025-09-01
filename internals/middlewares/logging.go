2931d329634fcd5412f20ffdf4cabb83e0530ec4
Tachera Sasi <tacherasasi@gmail.com>
2025-09-01 21:37:22 +0300

    Add logging middleware for request and response logging

--- internals/middlewares/logging.go
+++ internals/middlewares/logging.go
@@ -1,7 +1,20 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func LoggingMiddleware() gin.HandlerFunc {
-	
+	return func(c *gin.Context) {
+		// Log the request details
+		log.Printf("Request: %s %s", c.Request.Method, c.Request.URL.Path)
+
+		// Call the next handler
+		c.Next()
+
+		// Log the response details
+		log.Printf("Response: %d", c.Writer.Status())
+	}
 }

f4f986dde26df77553f5aa19bb3775b113eb3445
Tachera Sasi <tacherasasi@gmail.com>
2025-09-01 13:26:39 +0300

    Add configuration management with environment variable loading

---
+++ internals/config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type ConfigKey string
+
+const (
+    DBHostKey     ConfigKey = "DB_HOST"
+    DBPortKey     ConfigKey = "DB_PORT"
+	DBUserKey     ConfigKey = "DB_USER"
+    DBPasswordKey ConfigKey = "DB_PASSWORD"
+    DBNameKey     ConfigKey = "DB_NAME"
+    ServerPortKey ConfigKey = "SERVER_PORT"
+    JWTSecretKey  ConfigKey = "JWT_SECRET"
+)
+
+type config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	ServerPort string
+	JWTSecret  string
+}
+
+func LoadConfig() *config{
+	godotenv.Load()
+	return &config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "user"),
+		DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBName:     getEnv("DB_NAME", "dbname"),
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+		JWTSecret:  getEnv("JWT_SECRET", "secret"),
+	}
+}
+
+func (c *config) Get(key ConfigKey) string {
+    values := map[ConfigKey]string{
+        DBHostKey:     c.DBHost,
+        DBPortKey:     c.DBPort,
+        DBUserKey:     c.DBUser,
+        DBPasswordKey: c.DBPassword,
+        DBNameKey:     c.DBName,
+        ServerPortKey: c.ServerPort,
+        JWTSecretKey:  c.JWTSecret,
+    }
+    return values[key]
+}
+
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(string(key))
+	if !exists {
+		return defaultValue
+	}
+	return value
+}

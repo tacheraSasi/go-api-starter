88e2371c5fbbe7fd42ad34bab9f540651813c896
Tachera Sasi <tacherasasi@gmail.com>
2025-09-02 20:58:18 +0300

    Refactor user_repository.go to improve code organization and readability

--- internals/repositories/user_repository.go
+++ internals/repositories/user_repository.go
@@ -1,10 +1,44 @@
 package repositories
 
-import "github.com/tachRoutine/invoice-creator-api/internals/models"
+import (
+	"github.com/tachRoutine/invoice-creator-api/internals/models"
+	"gorm.io/gorm"
+)
 
 type UserRepository interface {
 	CreateUser(user *models.User) error
 	GetUserByID(id string) (*models.User, error)
 	UpdateUser(user *models.User) error
 	DeleteUser(id string) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (r *userRepository) CreateUser(user *models.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) GetUserByID(id string) (*models.User, error) {
+	var user models.User
+	if err := r.db.First(&user, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+
+func (r *userRepository) UpdateUser(user *models.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *userRepository) DeleteUser(id string) error {
+	return r.db.Delete(&models.User{}, "id = ?", id).Error
 }

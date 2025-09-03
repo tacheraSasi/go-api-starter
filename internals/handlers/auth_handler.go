4e7a3e91539775121ae02580990ce6bba99b73bc
Tachera Sasi <tacherasasi@gmail.com>
2025-09-03 21:02:12 +0300

    Implement request body reading and logging in Register method of AuthHandler

--- internals/handlers/auth_handler.go
+++ internals/handlers/auth_handler.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"io"
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tachRoutine/invoice-creator-api/internals/services"
+	"github.com/tachRoutine/invoice-creator-api/pkg/styles"
 )
 
 type AuthHandler struct {
@@ -16,6 +20,14 @@ func NewAuthHandler(service services.AuthService) *AuthHandler {
 }
 
 func (h *AuthHandler) Register(c *gin.Context) {
+	var requestBody = c.Request.Body
+	defer requestBody.Close()
+	bodyBytes, err := io.ReadAll(requestBody)
+	if err != nil {
+		log.Println("Failed to read request body:", err)
+		return
+	}
+	log.Println(styles.Request.Render(string(bodyBytes)))
 
 }
 

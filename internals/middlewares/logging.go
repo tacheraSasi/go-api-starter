e8e62093612e79d4ec5d298c78aa6ac03484dc7a
Tachera Sasi <tacherasasi@gmail.com>
2025-09-05 09:25:54 +0300

    Enhance logging middleware to include latency and response size metrics

--- internals/middlewares/logging.go
+++ internals/middlewares/logging.go
@@ -1,21 +1,36 @@
 package middlewares
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
-    "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 func LoggingMiddleware(logger *logrus.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// Start timer
+		startTime := time.Now()
 		logger.WithFields(map[string]interface{}{
-			"method": c.Request.Method,
-			"path":   c.Request.URL.Path,
+			"method":       c.Request.Method,
+			"path":         c.Request.URL.Path,
+			"client_ip":    c.ClientIP(),
+			"user_agent":   c.Request.UserAgent(),
+			"content_type": c.ContentType(),
+			"request_id":   c.GetHeader("X-Request-Id"),
 		}).Info("Request received")
 
 		c.Next()
+		// End timer
+		latency := time.Since(startTime)
 
 		logger.WithFields(map[string]interface{}{
-			"status": c.Writer.Status(),
+			"status":        c.Writer.Status(),
+			"latency_ms":    latency.Milliseconds(),
+			"response_size": c.Writer.Size(),
+			"client_ip":     c.ClientIP(),
+			"user_agent":    c.Request.UserAgent(),
+			"request_id":    c.GetHeader("X-Request-Id"),
 		}).Info("Response sent")
 	}
 }

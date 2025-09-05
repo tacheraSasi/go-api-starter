ef2fbc96ae8b3c9b50668a139be43c3177db7896
Tachera Sasi <tacherasasi@gmail.com>
2025-09-05 09:28:17 +0300

    Refactor logging middleware to log request details upon receipt and completion

--- internals/middlewares/logging.go
+++ internals/middlewares/logging.go
@@ -11,26 +11,21 @@ func LoggingMiddleware(logger *logrus.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Start timer
 		startTime := time.Now()
-		logger.WithFields(map[string]interface{}{
-			"method":       c.Request.Method,
-			"path":         c.Request.URL.Path,
-			"client_ip":    c.ClientIP(),
-			"user_agent":   c.Request.UserAgent(),
-			"content_type": c.ContentType(),
-			"request_id":   c.GetHeader("X-Request-Id"),
-		}).Info("Request received")
 
 		c.Next()
 		// End timer
 		latency := time.Since(startTime)
 
 		logger.WithFields(map[string]interface{}{
+			"method":        c.Request.Method,
+			"path":          c.Request.URL.Path,
+			"client_ip":     c.ClientIP(),
+			"user_agent":    c.Request.UserAgent(),
+			"content_type":  c.ContentType(),
+			"request_id":    c.GetHeader("X-Request-Id"),
 			"status":        c.Writer.Status(),
 			"latency_ms":    latency.Milliseconds(),
 			"response_size": c.Writer.Size(),
-			"client_ip":     c.ClientIP(),
-			"user_agent":    c.Request.UserAgent(),
-			"request_id":    c.GetHeader("X-Request-Id"),
-		}).Info("Response sent")
+		}).Info("Request completed")
 	}
 }
